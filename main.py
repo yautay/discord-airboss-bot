a284652334384964708aa8195672e8ad6c28b0cf
m.pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2022-10-14 21:42:43 +0200

    wip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from lib.Plotter import Plotter
-from lib.Plotter_AoA import PlotterAoA
+from modules.Plotter import Plotter
 
 file = "assets/AIRBOSS-CVN-75_Trapsheet-237 _ Nygus_FA-18C_hornet-0001.csv"
-plotter = PlotterAoA(file)
+plotter = Plotter(file)
+plotter.plot_aoa_graph(save=True)
 
---
+++ modules/Keys.py
@@ -0,0 +1,50 @@
+class KeysAirframes:
+    @staticmethod
+    def type():
+        return "type"
+
+    @staticmethod
+    def f18():
+        return "F/A-18C"
+
+    @staticmethod
+    def f14():
+        return "F-14"
+
+    @staticmethod
+    def av8():
+        return "AV-8B"
+
+
+class KeysAoA:
+    @staticmethod
+    def limits():
+        return "limits"
+
+    @staticmethod
+    def slo_hi():
+        return "slo_hi"
+
+    @staticmethod
+    def slo_med():
+        return "slo_med"
+
+    @staticmethod
+    def slo_lo():
+        return "slo_lo"
+
+    @staticmethod
+    def ok():
+        return "ok"
+
+    @staticmethod
+    def fast_lo():
+        return "fast_lo"
+
+    @staticmethod
+    def fast_med():
+        return "fast_med"
+
+    @staticmethod
+    def fast_hi():
+        return "fast_hi"
